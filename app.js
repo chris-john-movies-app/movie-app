08696c3afa8053c77c236b542b3e8ac672cca330
Chris Agold <christophersagold@gmail.com>
2021-01-15 14:19:30 -0600

    added nav bar and created poster div

--- app.js
+++ app.js
@@ -9,30 +9,30 @@ $(document).ready(function () {
     document.getElementById('movieResult').innerHTML = loader;
     let data = fetchData()
 
-    //while fetchData === undefined, loading= true, loading=false
-
-    function capString(word)
-    {
+    // Capitalize Movie Title //
+    function capString(word) {
         return word.charAt(0).toUpperCase() + word.slice(1);
     }
 
+    // FETCH DATA //
     function fetchData(data) {
         fetch(url).then(function (rep) {
             return rep.json()
         }).then(function (data) {
-            let result = `<h2> APP</h2>`;
+            let result = `<h2>Movie APP</h2>`;
             console.log(data);
             data.forEach(function (movie) {
-                result +=
-                    `<div class="parent-div">
-                    <h5> Movie ID: ${movie.id} </h5>
-                    <ul>
-                        <li>Movie name: ${capString(movie.title)}</li>
-                        <li>Movie year: ${movie.year}</li>
-                        <div><img src="${movie.poster}"></div>
-                         <button>Edit</button>
-                    </ul>
-                </div>`;
+                result += `
+<div class="card col-md-12" style="width: 18rem;">
+  <img class="card-img-top" src="${movie.poster}" alt="Card image cap">
+  <div class="card-body">
+    <h3 class="card-title">${capString(movie.title)}</h3>
+    <h5 class="card-title">${movie.year}</h5>
+    <h6 class="card-title">${movie.rating}</h6>
+    <p class="card-text">${movie.plot}</p>
+    <a href="#" class="btn btn-primary">Edit</a>
+  </div>
+</div>`
                 console.log(movie.title);
             });
             result += ``
@@ -77,7 +77,6 @@ $(document).ready(function () {
     //         });
     //     }
     //     remove();
-
 
     //******** EDIT MOVIE ***********//
     // getter function for updating movies (NOT WORKING)
